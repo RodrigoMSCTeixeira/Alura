46280e0c645c729f401d91d8692401f6cbdc8ec6
RodrigoMSCTeixeira <rodrigoteixeira@sefaz.pi.gov.br>
2023-07-31 13:52:28 -0300

    update

---
+++ organo/src/App.js
@@ -0,0 +1,13 @@
+import Banner from "./components/Banner/index.js";
+import Formulario from "./components/Formulario/index.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <Formulario />
+    </div>
+  );
+}
+
+export default App;
