e9bbeb4a57c3dc08d78049aa012fea5680d3691e
Rodrigo Teixeira <rteixerira@gmail.com>
2023-07-31 23:35:26 -0300

    update

---
+++ Javascript-classes-e-herancas/src/index.js
@@ -0,0 +1,10 @@
+import { Personagem } from "./modules/personagem.js";
+import { PersonagemView } from "./components/personagem-view.js";
+import { Mago } from "./modules/mago.js";
+
+const magoAntonio = new Mago("Antonio", 4, "fogo", 4, 3);
+const magaJulia = new Mago("Julia", 8, "gelo", 7, 10);
+
+const personagens = [magoAntonio, magaJulia];
+
+new PersonagemView(personagens).render();
---
+++ Javascript-classes-e-herancas/src/modules/personagem.js
@@ -0,0 +1,23 @@
+export class Personagem {
+  nome;
+  vida;
+  mana;
+  level;
+  tipo;
+  descricao;
+
+  constructor(nome, level) {
+    this.nome = nome;
+    this.level = level;
+    this.vida = 100;
+    this.mana = 100;
+  }
+
+  obterInsgnia() {
+    if (this.level >= 5) {
+      return `Implacável ${this.tipo}`;
+    }
+
+    return `${this.tipo} Iniciante`;
+  }
+}
