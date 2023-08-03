80017bba55ccedc8c7633f43924f606f5d9b1c5c
RodrigoMSCTeixeira <rodrigoteixeira@sefaz.pi.gov.br>
2023-08-03 11:06:45 -0300

    update

--- organo/src/components/CampoTexto/CampoTexto.js
+++ organo/src/components/CampoTexto/CampoTexto.js
@@ -1,10 +1,29 @@
+import { useState } from "react";
 import "./CampoTexto.css";
 
 export const CampoTexto = (props) => {
+  const placeholderModificada = `${props.placeholder}...`;
+
+  //let valor = "";
+
+  //Valor em si e um setter
+  const [valor, setValor] = useState("");
+
+  const aoDigitado = (evento) => {
+    props.aoAlterado(evento.target.value);
+    setValor(evento.target.value);
+    //valor = evento.target.value;
+  };
+
   return (
     <div className="campo-texto">
       <label>{props.label}</label>
-      <input placeholder={props.placeholder} />
+      <input
+        value={valor}
+        onChange={aoDigitado}
+        required={props.obrigatorio}
+        placeholder={placeholderModificada}
+      />
     </div>
   );
 };
