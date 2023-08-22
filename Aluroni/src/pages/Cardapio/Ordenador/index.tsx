460182a55ee12655ca30bbafded47402b519f43e
Rodrigo Teixeira <rteixerira@gmail.com>
2023-08-22 20:57:45 -0300

    update

--- Aluroni/src/pages/Cardapio/Ordenador/index.tsx
+++ Aluroni/src/pages/Cardapio/Ordenador/index.tsx
@@ -4,12 +4,14 @@ import opcoes from "./opcoes.json";
 import classNames from "classnames";
 import { MdKeyboardArrowUp, MdKeyboardArrowDown } from "react-icons/md";
 
-interface Props {
-  ordenador: string;
-  setOrdenador: React.Dispatch<React.SetStateAction<string>>;
+export type OpcoesOrdenador = "" | "porcao" | "qtd_pessoas" | "preco";
+
+interface IOrdenador {
+  ordenador: OpcoesOrdenador;
+  setOrdenador: React.Dispatch<React.SetStateAction<OpcoesOrdenador>>;
 }
 
-export default function Ordenador({ ordenador, setOrdenador }: Props) {
+export default function Ordenador({ ordenador, setOrdenador }: IOrdenador) {
   const [aberto, setAberto] = useState(false);
   const nomeOrdenador =
     ordenador && opcoes.find((opcao) => opcao.value === ordenador)?.nome;
@@ -38,7 +40,7 @@ export default function Ordenador({ ordenador, setOrdenador }: Props) {
           <div
             className={styles.ordenador__option}
             key={opcao.value}
-            onClick={() => setOrdenador(opcao.value)}
+            onClick={() => setOrdenador(opcao.value as OpcoesOrdenador)}
           >
             {opcao.nome}
           </div>
