7c0cfca4236e10fc05381a80799cf321978bb9bf
Rodrigo Teixeira <rteixerira@gmail.com>
2023-08-29 20:22:07 -0300

    update

---
+++ Cinetag/src/components/ComponenteModelo copy/index.js
@@ -0,0 +1,7 @@
+import styles from "./ComponenteModelo.module.css";
+
+function ComponenteModelo() {
+  return;
+}
+
+export default ComponenteModelo;
---
+++ Cinetag/src/components/Container/index.js
@@ -0,0 +1,7 @@
+import styles from "./Container.module.css";
+
+function Container({ children }) {
+  return <section className={styles.Container}>{children}</section>;
+}
+
+export default Container;
