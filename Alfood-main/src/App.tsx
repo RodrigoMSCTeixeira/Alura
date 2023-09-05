32291ded527a06fb22c88e7d053bc086f2e761b7
RodrigoMSCTeixeira <rodrigoteixeira@sefaz.pi.gov.br>
2023-09-05 13:57:00 -0300

    update

---
+++ Alfood-main/src/App.tsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./paginas/Home";
+import VitrineRestaurantes from "./paginas/VitrineRestaurantes";
+import AdministracaoRestaurantes from "./paginas/Adiministracao/Restaurantes/AdministracaoRestaurantes";
+import FormularioRestaurante from "./paginas/Adiministracao/Restaurantes/FormularioRestaurante";
+import PaginaBaseAdmin from "./paginas/Adiministracao/PaginaBaseAdmin";
+import AdministracaoPratos from "./paginas/Adiministracao/Pratos/AdministracaoPratos";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/restaurantes" element={<VitrineRestaurantes />} />
+
+      <Route path="/admin" element={<PaginaBaseAdmin />}>
+        <Route path="restaurantes" element={<AdministracaoRestaurantes />} />
+        <Route path="restaurantes/novo" element={<FormularioRestaurante />} />
+        <Route path="restaurantes/:id" element={<FormularioRestaurante />} />
+        <Route path="pratos" element={<AdministracaoPratos />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ Alfood-main/src/paginas/Adiministracao/Restaurantes/FormularioRestaurante.tsx
@@ -0,0 +1,134 @@
+import {
+  TextField,
+  Button,
+  Typography,
+  Box,
+  AppBar,
+  Container,
+  Toolbar,
+  Link,
+  Paper,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import http from "../../../http";
+import IRestaurante from "../../../interfaces/IRestaurante";
+import { Link as RouterLink } from "react-router-dom";
+
+const FormularioRestaurante = () => {
+  const parametros = useParams();
+
+  useEffect(() => {
+    if (parametros.id) {
+      http
+        .get<IRestaurante>(`restaurantes/${parametros.id}/`)
+        .then((resposta) => setNomeRestaurante(resposta.data.nome));
+    }
+  }, [parametros]);
+
+  const [nomeRestaurante, setNomeRestaurante] = useState("");
+
+  const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault();
+
+    if (parametros.id) {
+      http
+        .put(`restaurantes/${parametros.id}/`, {
+          nome: nomeRestaurante,
+        })
+        .then(() => alert("Restaurante atualizado"));
+    } else {
+      http
+        .post("restaurantes/", {
+          nome: nomeRestaurante,
+        })
+        .then(() => alert("Restaurante cadastrado"));
+    }
+  };
+
+  return (
+    <>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar>
+            <Typography variant="h6">Administração</Typography>
+
+            <Box
+              sx={{
+                display: "flex",
+                flexGrow: 1,
+              }}
+            >
+              <Link component={RouterLink} to="/admin/restaurantes">
+                <Button
+                  sx={{
+                    my: 2,
+                    color: "white",
+                  }}
+                >
+                  Restaurantes
+                </Button>
+              </Link>
+
+              <Link component={RouterLink} to="/admin/restaurantes/novo">
+                <Button
+                  sx={{
+                    my: 2,
+                    color: "white",
+                  }}
+                >
+                  Novo restaurante
+                </Button>
+              </Link>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+
+      <Box>
+        <Container maxWidth="lg" sx={{ mt: 1 }}>
+          <Paper sx={{ p: 2 }}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                flexGrow: 1,
+              }}
+            >
+              <Typography component="h1" variant="h6">
+                Formulário de Restaurantes
+              </Typography>
+              <Box
+                component="form"
+                sx={{ width: "100%" }}
+                onSubmit={aoSubmeterForm}
+              >
+                <TextField
+                  value={nomeRestaurante}
+                  onChange={(evento) => setNomeRestaurante(evento.target.value)}
+                  label="Nome do Restaurante"
+                  variant="standard"
+                  fullWidth
+                  required
+                />
+                <Button
+                  sx={{
+                    marginTop: 1,
+                  }}
+                  variant="outlined"
+                  type="submit"
+                  fullWidth
+                >
+                  Salvar
+                </Button>
+              </Box>
+            </Box>
+          </Paper>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+export default FormularioRestaurante;
