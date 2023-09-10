2b1c059d4ee1d2253ab2e53c1e672d307c98f58f
Rodrigo Teixeira <rteixerira@gmail.com>
2023-09-10 19:28:24 -0300

    update

--- Aluroni/src/index.tsx
+++ Aluroni/src/index.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'normalize.css';
+import ReactDOM from 'react-dom';
 import './index.css';
-import Router from './routes';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <Router />
-  </React.StrictMode>
+    <div> Aluroni </div>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
